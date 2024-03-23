fcfe5d83ea22f0d5c37778a3d73ee93c3438a8dc
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-24 03:59:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const usernameInput = document.getElementById('username');
+  const passwordInput = document.getElementById('password');
+  const rememberMeCheckbox = document.getElementById('rememberMe');
+  const submitButton = document.getElementById('submit');
+  const existingUserButton = document.getElementById('existing');
+
+  
+  const savedUsername = localStorage.getItem('username');
+  const savedPassword = localStorage.getItem('password');
+  if (savedUsername && savedPassword) {
+    existingUserButton.style.display = 'block';
+  }
+
+  
+  document.getElementById('loginForm').addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+    const rememberMe = rememberMeCheckbox.checked;
+
+    if (rememberMe) {
+      localStorage.setItem('username', username);
+      localStorage.setItem('password', password);
+    } else {
+      localStorage.removeItem('username');
+      localStorage.removeItem('password');
+    }
+
+    alert(`Logged in as`);
+  });
+
+  
+  existingUserButton.addEventListener('click', () => {
+    const savedUsername = localStorage.getItem('username');
+    alert(`Logged in as`);
+  });
