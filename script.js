46728da902cf90caa1a77e55e2318cedeb9c7715
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-03-25 23:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
 //your JS code here. If required.
   const usernameInput = document.getElementById('username');
-  const passwordInput = document.getElementById('password');
-  const rememberMeCheckbox = document.getElementById('rememberMe');
-  const submitButton = document.getElementById('submit');
-  const existingUserButton = document.getElementById('existing');
+            const passwordInput = document.getElementById('password');
+            const rememberMeCheckbox = document.getElementById('rememberMe');
 
-  
-  const savedUsername = localStorage.getItem('username');
-  const savedPassword = localStorage.getItem('password');
-  if (savedUsername && savedPassword) {
-    rememberMeCheckbox.checked = true;
-  }
+            document.getElementById('loginForm').addEventListener('submit', (event) => {
+                event.preventDefault(); 
+                const username = usernameInput.value;
+                const password = passwordInput.value;
+                const rememberMe = rememberMeCheckbox.checked;
 
-  
-  document.getElementById('loginForm').addEventListener('submit', (event) => {
-    event.preventDefault(); 
+                if (rememberMe) {
+                    localStorage.setItem('username', username);
+                    localStorage.setItem('password', password);
+                } else {
+                    localStorage.removeItem('username');
+                    localStorage.removeItem('password');
+                }
 
-    const username = usernameInput.value;
-    const password = passwordInput.value;
-    const rememberMe = rememberMeCheckbox.checked;
-
-    if (rememberMe) {
-      localStorage.setItem('username', username);
-      localStorage.setItem('password', password);
-    } else {
-      localStorage.removeItem('username');
-      localStorage.removeItem('password');
-    }
-    alert(`Logged in as`);
-  });
-
-  
- 
+                alert(`Logged in as ${username}`);
+            });
+        
